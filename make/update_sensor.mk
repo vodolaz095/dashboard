62f6b874c858d6f900be178aca53191b50915a71
Anatolij Ostroumov <anatolij@vodolaz095.ru>
2024-06-20 21:50:33 +0300

    upd sensor makefile target

--- make/update_sensor.mk
+++ make/update_sensor.mk
@@ -3,9 +3,8 @@ export timestamp=$(shell date "+%S")
 update_sensor/endpoint:
 	curl -v -H "Host: localhost" \
 		-H "Token: test321" \
-	    -H "Content-Type: application/x-www-form-urlencoded" \
-		-X POST \
-        -d "name=endpoint&value=21" \
+		-H "Content-Type: application/x-www-form-urlencoded" \
+		-d "name=endpoint&value=$(timestamp)" \
 		http://localhost:3000/update
 
 update_sensor/redis_subscriber:
