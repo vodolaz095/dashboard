32a711aef9fa80934d8c034f001ef50fe707b78b
Anatolij Ostroumov <anatolij@vodolaz095.ru>
2024-08-09 22:55:34 +0300

    expose /dump endpoint and additional metrics with stats only for development builds because it is meaningless in production

--- transport/webserver/metrics.go
+++ transport/webserver/metrics.go
@@ -18,17 +18,19 @@ func (tr *Transport) exposeMetrics() {
 		for i := range sensors {
 			fmt.Fprint(c.Writer, sensors[i].String())
 		}
-		fmt.Fprintln(c.Writer, "# HELP dashboard_queue_length Number of items in deferred queue for sensors updates")
-		fmt.Fprintln(c.Writer, "# TYPE dashboard_queue_length gauge")
-		fmt.Fprintf(c.Writer, "dashboard_queue_length %v %v\n", stats.QueueLength, time.Now().Unix())
+		if tr.Debug {
+			fmt.Fprintln(c.Writer, "# HELP dashboard_queue_length Number of items in deferred queue for sensors updates")
+			fmt.Fprintln(c.Writer, "# TYPE dashboard_queue_length gauge")
+			fmt.Fprintf(c.Writer, "dashboard_queue_length %v %v\n", stats.QueueLength, time.Now().Unix())
 
-		fmt.Fprintln(c.Writer, "# HELP dashboard_subscribers Number of subscribers for event feeds")
-		fmt.Fprintln(c.Writer, "# TYPE dashboard_subscribers gauge")
-		fmt.Fprintf(c.Writer, "dashboard_subscribers %v %v\n", stats.Subscribers, time.Now().Unix())
+			fmt.Fprintln(c.Writer, "# HELP dashboard_subscribers Number of subscribers for event feeds")
+			fmt.Fprintln(c.Writer, "# TYPE dashboard_subscribers gauge")
+			fmt.Fprintf(c.Writer, "dashboard_subscribers %v %v\n", stats.Subscribers, time.Now().Unix())
 
-		fmt.Fprintln(c.Writer, "# HELP dashboard_sensors_updated Number of sensors being updated now")
-		fmt.Fprintln(c.Writer, "# TYPE dashboard_sensors_updated gauge")
-		fmt.Fprintf(c.Writer, "dashboard_sensors_updated %v %v\n", stats.SensorsUpdatedNow, time.Now().Unix())
+			fmt.Fprintln(c.Writer, "# HELP dashboard_sensors_updated Number of sensors being updated now")
+			fmt.Fprintln(c.Writer, "# TYPE dashboard_sensors_updated gauge")
+			fmt.Fprintf(c.Writer, "dashboard_sensors_updated %v %v\n", stats.SensorsUpdatedNow, time.Now().Unix())
+		}
 		c.AbortWithStatus(http.StatusOK)
 	})
 }
